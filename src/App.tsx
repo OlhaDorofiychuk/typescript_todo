e3a4f49d242ef0fd86210b661d6f311132f8110b
OlhaDorofiychuk <olha.d@yahoo..com>
2023-01-05 10:52:34 +0000

    todo list added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 import ImputComponent from './components/ImputComponent';
+import { Todo } from './components/item';
+import TodoList from './components/TodoList';
 
 const App: React.FC=()=>{
-  const [todo, setTodo] = useState<string>('')
+  const [todo, setTodo] = useState<string>('');
+  const [todos, seTodos] = useState<Todo[]>([]);
+  const handleAdd =(e:React.FormEvent)=>{
+    e.preventDefault()
+    if(todo) {
+      seTodos([...todos,{id: Date.now(), todo, isDone: false}])
+      setTodo('')
+    }
+  }
   console.log(todo)
+  console.log('list',todos)
   return (
     <div className="App">
       <span className='heading'>My ToDo</span>
-      <ImputComponent todo={todo} setTodo={setTodo}/>
+      <ImputComponent todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList />
+      {todos.map(task=>(
+        <li key={task.id}>{task.todo}</li>
+      ))}
     </div>
   );
 }
--- src/components/ImputComponent.tsx
+++ src/components/ImputComponent.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>
+    handleAdd:(e:React.FormEvent)=>void;
 }
-const ImputComponent = ({todo, setTodo}: Props) => {
+const ImputComponent = ({todo, setTodo, handleAdd}: Props) => {
   return (
-  <form className='input'>
+  <form className='input' onSubmit={handleAdd}>
     <input className='input_field' type='input' value={todo} onChange={(e)=>setTodo(e.target.value)}placeholder='Enter new task'></input>
     <button className='input_submt' type='submit'>Add</button>
   </form>
