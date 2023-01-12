1d3cc3c04e4856783c351e425856db28fe05289c
OlhaDorofiychuk <olha.d@yahoo..com>
2023-01-12 12:48:48 +0000

    before installing beautiful-dnd

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -17,7 +17,7 @@ const TodoList: React.FC<Props> = ({todos,setTodos}) => {
                 <SingleTodo todo={todo} todos={todos} key={todo.id} setTodos={setTodos}/>
             ))}
         </div>
-        <div className="list_todo remove">
+        <div className="list_todos completed">
         <span className="list_todos_heading">Completed Tasks</span>
             {todos.map((todo)=>(
                 <SingleTodo todo={todo} todos={todos} key={todo.id} setTodos={setTodos}/>
